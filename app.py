f645b3ff9622b312b7f10bab58831b0569e71012
Simen Daehlin <simen.dehlin@gmail.com>
2017-06-26 20:59:11 +0100

    Updated 1.0.1

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template, request
-from flaskext.mysql import MySQL
-
-mysql = MySQL()
+from flask import Flask, render_template
+from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.json_util import dumps
 app = Flask(__name__)
-# Adding Mysql Connection
-app.config['MYSQL_DATABASE_USER'] = 'exam'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'exam123'
-app.config['MYSQL_DATABASE_DB'] = 'exam'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+
+# Adding MongoDB Connection
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'movies'
+COLLECTION_NAME = 'projects'
+FIELDS = {
+    '_id': True,
+    'MovieName': True,
+    'Genre1': True,
+    'Genre2': True,
+    'Genre3': True,
+    'Genre4': True,
+    'Genre5': True,
+    'Genre6': True
+
+}
 
 # For auto Reloading
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -24,5 +35,21 @@ def overview():
 @app.route('/movie')
 def movie():
     return render_template('movie.html')
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
+@app.route('/api/data/data.json')
+def apidata():
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME][COLLECTION_NAME]
+    projects = collection.find(projection=FIELDS)
+    json_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects, default=json_util.default)
+    connection.close()
+    return json_projects
 if __name__== '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    print "Server Running"
     app.run(debug=True)
---
+++ static/js/custom.js
@@ -0,0 +1,31 @@
+d3.json("/api/data/data.json", function(error, json) {
+    if (error) return console.log(error);
+    d3.select("#vis").append("svg")
+      .data(json)
+      .text(function(d){
+          return d.MovieName;
+      });
+});
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+getJSON('/api/data/data.json',
+function(err, data) {
+  if (err != null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    // console.log(data.length); // For debugging
+    document.getElementById("entryamount").innerHTML = data.length;
+  }
+});
